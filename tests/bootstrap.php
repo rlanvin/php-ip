fc29be4418f3a6b65b5e12fd9df9544c7ff86dd5
rlanvin <remi@cloudconnected.fr>
2014-04-20 11:07:55 +0300

    Adding Composer information

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
-require __DIR__.'/../src/IP.php';
-require __DIR__.'/../src/IPv4.php';
-require __DIR__.'/../src/IPv6.php';
-require __DIR__.'/../src/IPBlock.php';
-require __DIR__.'/../src/IPv4Block.php';
-require __DIR__.'/../src/IPv6Block.php';
-require __DIR__.'/../src/IPBlockIterator.php';
+require __DIR__.'/../vendor/autoload.php';
