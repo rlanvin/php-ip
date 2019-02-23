cf2c10ba47c360227dd43a259a2674cd2c6c0e4d
Samuel Williams <samuel.williams8@gmail.com>
2019-02-24 07:48:46 +1000

    Write failing test case for getSubblocks()

--- tests/IPBlockTest.php
+++ tests/IPBlockTest.php
@@ -189,7 +189,17 @@ class IPBlockTest extends \PHPUnit_Framework_TestCase
 
     public function testGetSubblocks()
     {
-        // todo
+        $block = IPBlock::create('192.168.8.0/24');
+        $subnets = $block->getSubblocks('/28');
+
+        $this->assertCount(16, $subnets);
+        $this->assertEquals('192.168.8.0', $subnets->current()->getFirstIp()->humanReadable());
+        $this->assertEquals(28, $subnets->current()->getPrefix());
+
+        $subnets->next();
+        $subnets->next();
+
+        $this->assertEquals('192.168.8.32/28', $subnets->current()->getGivenIpWithPrefixlen());
     }
 
     public function testGetSuper()
